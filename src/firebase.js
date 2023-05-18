a51182918fd809ac04c4a557e002dd91aa4ba767
repegfrost <61025991+repegfrost@users.noreply.github.com>
2023-05-18 14:31:41 +0530

    added firebase config

--- src/firebase.js
+++ src/firebase.js
@@ -3,9 +3,16 @@ import 'firebase/firestore';
 import 'firebase/auth';
 import 'firebase/storage';
 
-const firebaseConfig = { 
-    // Add your config here
+const firebaseConfig = {
+    apiKey: "AIzaSyA0HdVWV7f8_uzgZ6VnNs1ZySxbGw9d43A",
+    authDomain: "next-fire-443b9.firebaseapp.com",
+    projectId: "next-fire-443b9",
+    storageBucket: "next-fire-443b9.appspot.com",
+    messagingSenderId: "81739948154",
+    appId: "1:81739948154:web:f50ec5b0b6ae1faf57dbc3",
+    measurementId: "G-280YFTGW2H"
 };
+  
 
 firebase.initializeApp(firebaseConfig);
 
